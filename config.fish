288b8d38d41898b42662648ba34156996138165f
mecaota <24618982+mecaota@users.noreply.github.com>
2022-08-08 10:55:34 +0900

    起動中のターミナルによってanyenvを起動仕分けるスクリプトを追加

--- config.fish
+++ config.fish
@@ -1,5 +1,13 @@
-# path
+if status is-interactive
+  # check running terminal and initialize
+  set _PPID $(ps -o ppid -p $fish_pid | tail -n 1)
+  if ps -p $_PPID | grep -qs Code.app
+    echo ":::run from vscode terminal:::"
+    bass source (anyenv init -|psub) || true
+  else
+    source (anyenv init -|psub)
+  end
 
-# initialize
-status --is-interactive; and source (anyenv init -|psub)
-
+  # path
+  fish_add_path $HOME/.anyenv/bin
+end
